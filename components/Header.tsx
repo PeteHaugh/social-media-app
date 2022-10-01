8ffc174388cff69fe8222627176ff2fc4ed106b8
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-01 13:25:38 +0100

    Added form validation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,105 @@
-import React from 'react'
-import Image from 'next/image';
-import { BellIcon, ChatBubbleLeftIcon, ChevronDownIcon, GlobeAltIcon, HomeIcon, MagnifyingGlassIcon, PlusIcon, SparklesIcon, SpeakerWaveIcon, VideoCameraIcon } from '@heroicons/react/24/solid'
-import { StarIcon, Bars3BottomRightIcon } from '@heroicons/react/24/outline';
-import { signIn, signOut, useSession } from 'next-auth/react';
-
+import React from "react";
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatBubbleLeftIcon,
+  ChevronDownIcon,
+  GlobeAltIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerWaveIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/solid";
+import { StarIcon, Bars3BottomRightIcon } from "@heroicons/react/24/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
-
   const { data: session } = useSession();
 
   return (
-    <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
-        <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
-            <Image objectFit="contain" src="https://links.papareact.com/fqy" layout="fill"
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search Box */}
+      <form className="flex flex-1 items-center space-x-2 border-gray-200 rounded-sm bg-gray-100 px-3 py-1">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border-gray-100" />
+        <ChatBubbleLeftIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerWaveIcon className="icon" />
+      </div>
+
+      <div className="mx-5 flex items-center lg:hidden">
+        <Bars3BottomRightIcon className="icon" />
+      </div>
+
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              layout="fill"
+              src="https://links.papareact.com/23l"
+              alt=""
             />
-        </div>
-
-        <div className='mx-7 flex items-center xl:min-w-[300px]'>
-            <HomeIcon className='h-5 w-5'/>
-            <p className='ml-2 hidden flex-1 lg:inline'>Home</p>
-            <ChevronDownIcon className='h-5 w-5'/>
-        </div>
-
-        {/* Search Box */}
-        <form className='flex flex-1 items-center space-x-2 border-gray-200 rounded-sm bg-gray-100 px-3 py-1'>
-            <MagnifyingGlassIcon className='h-6 w-6 text-gray-400'/>
-            <input className="flex-1 bg-transparent outline-none" type="text" placeholder="Search Reddit" />
-            <button type='submit' hidden />
-        </form>
-
-        <div className='mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex'>
-          <SparklesIcon className='icon'/>
-          <GlobeAltIcon className='icon'/>
-          <VideoCameraIcon className='icon'/>
-          <hr className='h-10 border-gray-100'/>
-          <ChatBubbleLeftIcon className='icon'/>
-          <BellIcon className='icon'/>
-          <PlusIcon className='icon'/>
-          <SpeakerWaveIcon className='icon'/>
-        </div>
-
-        <div className='mx-5 flex items-center lg:hidden'>
-          <Bars3BottomRightIcon className='icon' />
-        </div>
-
-        {session ? (
-          <div onClick={() => signOut()} className='hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer'>
-            <div className='relative h-5 w-5 flex-shrink-0'>
-              <Image objectFit='contain' layout='fill' src="https://links.papareact.com/23l" alt="" />
-            </div>
-        <div className='flex-1 text-xs'>
-          <p>{session?.user?.name}</p>
-          <p className='text-gray-400'>1 Karma</p>
-        </div>
-          <ChevronDownIcon className='h-5 flex-shrink-0 text-gray-400' />
-        </div>
-
-        
-        ) : (
-        <div onClick={() => signIn()} className='hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer'>
-          <div className='relative h-5 w-5 flex-shrink-0'>
-            <Image objectFit='contain' layout='fill' src="https://links.papareact.com/23l" alt="" />
           </div>
-          
-        <div>
-          <p className='text-gray-400'>Sign in</p>
+          <div className="flex-1 text-xs">
+            <p>{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              layout="fill"
+              src="https://links.papareact.com/23l"
+              alt=""
+            />
+          </div>
+
+          <div>
+            <p className="text-gray-400">Sign in</p>
+          </div>
         </div>
-        )}
+      )}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,23 +1,109 @@
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "./Avatar";
+import { LinkIcon, PhotoIcon } from "@heroicons/react/24/outline";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreddit: string;
+};
 
 function PostBox() {
   const { data: session } = useSession();
+  const [imageBoxOpen, setImageBoxOpen] = useState(false);
 
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = () => {
+
+  }
+  
   return (
-    <form>
+    <form className="sticky top-16 z-50 bg-white border rounded-md p-2">
       <div className="flex items-center space-x-3">
         <Avatar />
         <input
-            disabled={!session}
-            className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+          {...register("postTitle", { required: true })}
+          disabled={!session}
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
           type="text"
           placeholder={
             session ? "Create a post by entering a title" : "Sign in to post"
           }
         />
+        <PhotoIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 text-gray-300 cursor-pointer ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
+        <LinkIcon className="h-6 text-gray-600" />
       </div>
+
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body</p>
+
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("subreddit", { required: true })}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="Optional..."
+              />
+            </div>
+          )}
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === "required" && <p>Title required</p>}
+              {errors.subreddit?.type === "required" && (
+                <p>Subreddit required</p>
+              )}
+            </div>
+          )}
+
+          {!!watch("postTitle") && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create post
+            </button>
+          )}
+        </div>
+      )}
     </form>
   );
 }
